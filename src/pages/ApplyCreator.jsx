c03dbe7e025fab93faa49455ffcb0a31621538d9
Luka Lazic <lukalazic2017@gmail.com>
2026-03-11 23:45:22 -0700

    Update

--- src/pages/ApplyCreator.jsx
+++ src/pages/ApplyCreator.jsx
@@ -1,4 +1,5 @@
-
+import React, { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
 
 export default function ApplyCreator() {
   const [user, setUser] = useState(null);
